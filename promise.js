a2def71f5258a4e49a7aa1965d8b9140cc3399ad
Oleg <oleg.tcm@gmail.com>
2024-08-12 20:53:09 +0300

    a

--- promise.js
+++ promise.js
@@ -25,3 +25,25 @@ let myPromise = new Promise(function (resolve, reject) {
 });
 
 
+fetch(url)
+    .then((response) => {
+        if (response.status >= 200 && response.status < 400) {
+            return response.json();
+        } else {
+            console.log(`${response.statusText} : ${response.status} error`);
+        }
+    })
+    .then((data) => {
+        console.log(data)
+    }).catch((error) => {
+        console.log(error);
+    })
+
+let isLoading = true
+sendForm().then((res) => {
+    alert('ok')
+}).catch((err) => {
+    alert(`Ошибка: ${err.message}`)
+}).finally(() => {
+    isLoading = false
+})
